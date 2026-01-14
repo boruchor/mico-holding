267746769a1b680db89ed5831c35203acc9d0b10
qgmbek <100270972+qgmbek@users.noreply.github.com>
2026-01-14 19:51:53 +0500

    Initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,34 @@
+import "./Hero.css";
+import bg from "../../assets/bg.webp";
+import logo from "../../assets/logo.png";
+import { motion } from "framer-motion";
+
+export default function Hero() {
+  return (
+    <motion.div
+      className="hero-wrapper"
+      style={{ backgroundImage: `url(${bg})` }}
+      initial={{ opacity: 0, scale: 1.05 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 1, ease: "easeOut" }}
+    >
+      <motion.img
+        src={logo}
+        alt=""
+        className="hero-logo"
+        initial={{ opacity: 0, y: -30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.6, duration: 0.8, ease: "easeOut" }}
+      />
+
+      <motion.div
+        className="hero-text"
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 1, duration: 0.8, ease: "easeOut" }}
+      >
+        UNDER MAINTENANCE
+      </motion.div>
+    </motion.div>
+  );
+}
